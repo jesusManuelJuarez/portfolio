535c48c80b96e13f50a594cd9318f82094468677
jesusManuelJuarez <chuy.america.az@gmail.com>
2023-08-15 16:37:00 -0600

    fisrt commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const projects = document.querySelectorAll('.project');
+
+projects.forEach(project => {
+  const btn = project.querySelector('.btn');
+  const details = project.querySelector('.details');
+  const closeButton = details.querySelector('.close-button'); // Selecciona el botón de cierre dentro de la sección de detalles
+
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    project.classList.toggle('active');
+  });
+
+  closeButton.addEventListener('click', () => {
+    project.classList.remove('active');
+  });
+
+  details.addEventListener('click', (event) => {
+    if (event.target === details) {
+      project.classList.remove('active');
+    }
+  });
+});
