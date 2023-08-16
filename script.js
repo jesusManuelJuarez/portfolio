f9ebd66eb585a21671619f9f1696fd4de4b41deb
jesusManuelJuarez <chuy.america.az@gmail.com>
2023-08-15 23:35:28 -0600

    update of change UI and UX

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('transparent');
+  } else {
+    header.classList.remove('transparent');
+  }
+});
+
 const projects = document.querySelectorAll('.project');
 
 projects.forEach(project => {
   const btn = project.querySelector('.btn');
   const details = project.querySelector('.details');
-  const closeButton = details.querySelector('.close-button'); // botón de cierre dentro de la sección de detalles
+
 
   btn.addEventListener('click', (event) => {
     event.preventDefault();
     project.classList.toggle('active');
-  });
 
-  closeButton.addEventListener('click', () => {
-    project.classList.remove('active');
+    if (project.classList.contains('active')) {
+      btn.textContent = 'Ver Menos';
+    } else {
+      btn.textContent = 'Ver Detalles';
+    }
   });
 
   details.addEventListener('click', (event) => {
     if (event.target === details) {
       project.classList.remove('active');
+      btn.textContent = 'Ver Detalles';
     }
   });
 });
+
+
+
+
